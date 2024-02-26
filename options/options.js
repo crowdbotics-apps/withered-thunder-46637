1599fce2090ad68a355cd7a35b31f3eb50de819d
Crowdbotics <team@crowdbotics.com>
2024-02-26 15:52:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-thunder-46637",
+  url: "https://withered-thunder-46637.botics.co",
+  api: "https://withered-thunder-46637.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
